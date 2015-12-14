c27bbdb8c9010ff4e88ceb143134069c425a4a7a
ChrisBoon <cboon27@gmail.com>
2015-12-14 00:30:27 -0500

    Add JSON data for Chris Boon
    Signed-off-by: ChrisBoon <cboon27@gmail.com>

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,137 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+// My data as JSON objects:
+
+var bio = {
+  "name" : "Chris Boon",
+  "role" : "UX/UI Designer",
+  "contacts" : {
+    "mobile" : "646-327-4724",
+    "email" : "cboon27@gmail.com",
+    "github" : "https://github.com/ChrisBoon/",
+    "codepen" : "http://codepen.io/chrisboon27/",
+    "stackoverflow" : "http://stackoverflow.com/users/2175571/chris-boon",
+    "location" : "NYC"
+  },
+  "welcomeMessage" : "Hello, I guess..."
+  "skills" : [
+    "array",
+    "of",
+    "strings"
+  ],
+  "biopic" : "http://gravatar.com/avatar/3df0b05a374aa747d45136d8d22342ab?s=512",
+  "display" : function(){
+
+  }
+};
+
+var education = {
+  "schools" : [
+    {
+      "name" : "Falmouth University",
+      "location" : "Falmouth, Cornwall, UK",
+      "degree" : "BA (Hons) Graphic Design",
+      "dates" : 2007,
+      "url" : "http://www.falmouth.ac.uk/"
+    }
+  ],
+  "onlineCourses" : [
+    {
+      "title" : "jQuery",
+      "school" : "Codecademy",
+      "date" : 2012,
+      "url" : "https://www.codecademy.com"
+    },
+    {
+      "title" : "PHP",
+      "school" : "Codecademy",
+      "date" : 2013,
+      "url" : "https://www.codecademy.com"
+    },
+    {
+      "title" : "HTML & CSS",
+      "school" : "Codecademy",
+      "date" : 2012,
+      "url" : "https://www.codecademy.com"
+    },
+    {
+      "title" : "Javascript",
+      "school" : "Codecademy",
+      "date" : 2012,
+      "url" : "https://www.codecademy.com"
+    },
+    {
+      "title" : "Learn AngularJS",
+      "school" : "Codecademy",
+      "date" : 2015,
+      "url" : "https://www.codecademy.com"
+    },
+    {
+      "title" : "Learn the Command Line",
+      "school" : "Codecademy",
+      "date" : 2015,
+      "url" : "https://www.codecademy.com"
+    },
+    {
+      "title" : "Python",
+      "school" : "Codecademy",
+      "date" : 2013,
+      "url" : "https://www.codecademy.com"
+    },
+    {
+      "title" : "Ruby",
+      "school" : "Codecademy",
+      "date" : 2013,
+      "url" : "https://www.codecademy.com"
+    },
+
+  ],
+  "display" : function(){
+
+  }
+}
+
+var work = {
+  "jobs": [
+    {
+     "employer" : "Oxford University Press",
+     "title" : "UX / UI Designer",
+     "location" : "New York, NY",
+     "dates" : "2012 - Present",
+     "description" : "In-house lead for UX and UI design for all new projects developed by the New York group. Heavily involved in product creation from original concept through to final execution. Worked on several award-winning educational products including websites, Online Learning Platforms, and heavily interactive iBooks."
+    },
+    {
+     "employer" : "Oxford University Press",
+     "title" : "Senior Digital Designer",
+     "location" : "Oxford, UK",
+     "dates" : "2011-2012",
+     "description" : "Designed and coded front end for ambitious new online learning platform. Developed designs for dozens of interactive games."
+    },
+    {
+     "employer" : "Oxford University Press",
+     "title" : "Designer",
+     "location" : "Oxford, UK",
+     "dates" : "2007-2011",
+     "description" : "Designed multiple award-winning, market-leading English Language Teaching books, including 3 years on Headway, the world's best-selling ELT coursebook. Also lead designer on Network, the best-selling course for the Italian Secondary school market - developed the book and was UI designer for the accompanying app."
+    }
+  ],
+  "display" : function(){
+
+  }
+}
+
+var projects = {
+  "projects" : [
+    {
+      "title" : "Online Practice v1.3.0",
+      "dates" : "2014"
+      "description" : "add text here when sure what to include",
+      "images" : [
+        "url1",
+        "url2",
+        "url3"
+      ]
+    }
+  ],
+  "display" : function(){
+
+  }
+}
